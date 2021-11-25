3e060b956873aa1919a2df58bff362d60cf15545
Jesús Caro <jfcarocota@gmail.com>
2021-11-25 14:44:46 -0700

    Updates

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ let camera, scene, raycaster, renderer;
 let room;
 
 let controller, controllerGrip;
-let INTERSECTED;
-const tempMatrix = new THREE.Matrix4();
 
 init();
 animate();
@@ -33,7 +31,6 @@ function init() {
     new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ).translate( 0, 3, 0 ),
     new THREE.LineBasicMaterial( { color: 0x808080 } )
   );
-  //scene.add( room );
 
   scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
 
@@ -41,32 +38,6 @@ function init() {
   light.position.set( 1, 1, 1 ).normalize();
   scene.add( light );
 
-  //const geometry = new THREE.BoxGeometry( 0.15, 0.15, 0.15 );
-
-  /*for ( let i = 0; i < 200; i ++ ) {
-
-    const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-
-    object.position.x = Math.random() * 4 - 2;
-    object.position.y = Math.random() * 4;
-    object.position.z = Math.random() * 4 - 2;
-
-    object.rotation.x = Math.random() * 2 * Math.PI;
-    object.rotation.y = Math.random() * 2 * Math.PI;
-    object.rotation.z = Math.random() * 2 * Math.PI;
-
-    object.scale.x = Math.random() + 0.5;
-    object.scale.y = Math.random() + 0.5;
-    object.scale.z = Math.random() + 0.5;
-
-    object.userData.velocity = new THREE.Vector3();
-    object.userData.velocity.x = Math.random() * 0.01 - 0.005;
-    object.userData.velocity.y = Math.random() * 0.01 - 0.005;
-    object.userData.velocity.z = Math.random() * 0.01 - 0.005;
-
-    room.add( object );
-
-  }*/
 
   const geometry = new THREE.BoxGeometry();
   const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
@@ -81,8 +52,6 @@ function init() {
   renderer.outputEncoding = THREE.sRGBEncoding;
   renderer.xr.enabled = true;
   container.appendChild( renderer.domElement );
-
-  //
 
   function onSelectStart() {
 
@@ -118,8 +87,6 @@ function init() {
   scene.add( controllerGrip );
 
   window.addEventListener( 'resize', onWindowResize );
-
-  //
 
   document.body.appendChild( VRButton.createButton( renderer ) );
 
@@ -160,8 +127,6 @@ function onWindowResize() {
 
 }
 
-//
-
 function animate() {
 
   renderer.setAnimationLoop( render );
@@ -171,86 +136,6 @@ function animate() {
 function render() {
 
   const delta = clock.getDelta() * 60;
-
-  /*if ( controller.userData.isSelecting === true ) {
-
-    const cube = room.children[ 0 ];
-    room.remove( cube );
-
-    cube.position.copy( controller.position );
-    cube.userData.velocity.x = ( Math.random() - 0.5 ) * 0.02 * delta;
-    cube.userData.velocity.y = ( Math.random() - 0.5 ) * 0.02 * delta;
-    cube.userData.velocity.z = ( Math.random() * 0.01 - 0.05 ) * delta;
-    cube.userData.velocity.applyQuaternion( controller.quaternion );
-    room.add( cube );
-
-  }*/
-
-  // find intersections
-
-  /*tempMatrix.identity().extractRotation( controller.matrixWorld );
-
-  raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
-  raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
-
-  const intersects = raycaster.intersectObjects( room.children, false );
-
-  if ( intersects.length > 0 ) {
-
-    if ( INTERSECTED != intersects[ 0 ].object ) {
-
-      if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-      INTERSECTED = intersects[ 0 ].object;
-      INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-      INTERSECTED.material.emissive.setHex( 0xff0000 );
-
-    }
-
-  } else {
-
-    if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-    INTERSECTED = undefined;
-
-  }*/
-
-  // Keep cubes inside room
-
-  /*for ( let i = 0; i < room.children.length; i ++ ) {
-
-    const cube = room.children[ i ];
-
-    cube.userData.velocity.multiplyScalar( 1 - ( 0.001 * delta ) );
-
-    cube.position.add( cube.userData.velocity );
-
-    if ( cube.position.x < - 3 || cube.position.x > 3 ) {
-
-      cube.position.x = THREE.MathUtils.clamp( cube.position.x, - 3, 3 );
-      cube.userData.velocity.x = - cube.userData.velocity.x;
-
-    }
-
-    if ( cube.position.y < 0 || cube.position.y > 6 ) {
-
-      cube.position.y = THREE.MathUtils.clamp( cube.position.y, 0, 6 );
-      cube.userData.velocity.y = - cube.userData.velocity.y;
-
-    }
-
-    if ( cube.position.z < - 3 || cube.position.z > 3 ) {
-
-      cube.position.z = THREE.MathUtils.clamp( cube.position.z, - 3, 3 );
-      cube.userData.velocity.z = - cube.userData.velocity.z;
-
-    }
-
-    //cube.rotation.x += cube.userData.velocity.x * 2 * delta;
-    //cube.rotation.y += cube.userData.velocity.y * 2 * delta;
-    //cube.rotation.z += cube.userData.velocity.z * 2 * delta;
-
-  }*/
 
   renderer.render( scene, camera );
 
