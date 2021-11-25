728dabcd926c01dbe54b920f076f73d3d8495dd6
Jesús Caro <jfcarocota@gmail.com>
2021-11-25 15:00:26 -0700

    Updates

--- index.js
+++ index.js
@@ -24,13 +24,6 @@ function init() {
   camera.position.set( 0, 1.6, 50 );
   scene.add( camera );
 
-  room = new THREE.LineSegments(
-    new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ).translate( 0, 3, 0 ),
-    new THREE.LineBasicMaterial( { color: 0x808080 } )
-  );
-
-  scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
-
   const light = new THREE.DirectionalLight( 0xffffff );
   light.position.set( 1, 1, 1 ).normalize();
   scene.add( light );
