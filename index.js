c95ee9a225f9e81a8a4410bce564f327226c38e6
Jesús Caro <jfcarocota@gmail.com>
2021-11-25 14:53:11 -0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import * as THREE from './js/build/three.module.js';
 import { BoxLineGeometry } from './js/examples/jsm/geometries/BoxLineGeometry.js';
 import { VRButton } from './js/examples/jsm/webxr/VRButton.js';
-import { XRControllerModelFactory } from './js/examples/jsm/webxr/XRControllerModelFactory.js';
 
 const clock = new THREE.Clock();
 
@@ -9,8 +8,6 @@ let container;
 let camera, scene, raycaster, renderer;
 
 let room;
-
-//let controller, controllerGrip;
 
 init();
 animate();
@@ -53,68 +50,9 @@ function init() {
   renderer.xr.enabled = true;
   container.appendChild( renderer.domElement );
 
-  function onSelectStart() {
-
-    this.userData.isSelecting = true;
-
-  }
-
-  function onSelectEnd() {
-
-    this.userData.isSelecting = false;
-
-  }
-
-  /*controller = renderer.xr.getController( 0 );
-  controller.addEventListener( 'selectstart', onSelectStart );
-  controller.addEventListener( 'selectend', onSelectEnd );
-  controller.addEventListener( 'connected', function ( event ) {
-
-    this.add( buildController( event.data ) );
-
-  } );
-  controller.addEventListener( 'disconnected', function () {
-
-    this.remove( this.children[ 0 ] );
-
-  } );
-  scene.add( controller );
-
-  const controllerModelFactory = new XRControllerModelFactory();
-
-  controllerGrip = renderer.xr.getControllerGrip( 0 );
-  controllerGrip.add( controllerModelFactory.createControllerModel( controllerGrip ) );
-  scene.add( controllerGrip );*/
-
   window.addEventListener( 'resize', onWindowResize );
 
   document.body.appendChild( VRButton.createButton( renderer ) );
-
-}
-
-function buildController( data ) {
-
-  let geometry, material;
-
-  switch ( data.targetRayMode ) {
-
-    case 'tracked-pointer':
-
-      geometry = new THREE.BufferGeometry();
-      geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 1 ], 3 ) );
-      geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( [ 0.5, 0.5, 0.5, 0, 0, 0 ], 3 ) );
-
-      material = new THREE.LineBasicMaterial( { vertexColors: true, blending: THREE.AdditiveBlending } );
-
-      return new THREE.Line( geometry, material );
-
-    case 'gaze':
-
-      geometry = new THREE.RingGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
-      material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
-      return new THREE.Mesh( geometry, material );
-
-  }
 
 }
 
